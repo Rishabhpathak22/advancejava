f67183c17c9b111bc06da1597c6553c9916f3186
Rishabh Pathak <72240800+Rishabhpathak22@users.noreply.github.com>
2021-03-08 19:24:18 +0530

    Create jframeDemo2.java

---
+++ jframeDemo2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+class JframeDemo1 {
+    public static void main(String[] args) {
+        JFrame ob2= new JFrame("Rishabh's Frame");
+        ob2.setSize(280,300);
+        ob2.setVisible(true);
+    }
+}
